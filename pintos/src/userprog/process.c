db404481076ef789615a3a7ad052e1ce66c1e994
Coby Zhong <coby.zhong@berkeley.edu>
2020-03-04 06:28:38 +0000

    Revert "my changes for exec-missing" Prevents multi-oom from hanging
    This reverts commit fa883b311fadef79d65eaa5906fa51e37276fec3.

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -131,12 +131,6 @@ start_process (void *wrapper)
   if_.eflags = FLAG_IF | FLAG_MBS;
   success = load (args[0], &if_.eip, &if_.esp);
 
-  if (!success) {
-    palloc_free_page (args[0]);
-    sema_up(&thread_current()->self_status->load);
-    thread_exit();
-    return;
-  }
   /* push args strings onto stack */
   int arg_len;
   int arg_addrs[argc];
@@ -181,9 +175,9 @@ start_process (void *wrapper)
   /* If load failed, quit. */
   palloc_free_page (args[0]);
   sema_up(&thread_current()->self_status->load);
-  /*if (!success) {
+  if (!success) {
     thread_exit();
-  }*/
+  }
 
   // Successful load, increment ref_cnt before waking up parent
   lock_acquire(&thread_current()->self_status->ref_lock);
@@ -260,9 +254,9 @@ process_exit (void)
   struct list_elem *e;
   struct list *children_status = &cur->children_status;
   for (e = list_begin(children_status); e != list_end(children_status); e = list_next(e)) {
-    /*if (e->next == NULL) { // just skips the for loop altogether bc list_next is not working
+    if (e->next == NULL) { // just skips the for loop altogether bc list_next is not working
       break;
-    }*/
+    }
     struct child_status *curr_child = list_entry (e, struct child_status, elem);
     lock_acquire(&(curr_child->ref_lock));
     curr_child->ref_cnt -= 1;

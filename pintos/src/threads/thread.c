7469dfd99e8891851ecbdf15152e836e7f2b2e4f
Coby Zhong <coby.zhong@berkeley.edu>
2020-03-04 03:47:49 +0000

    Prepare to merge remote changes

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -185,13 +185,10 @@ thread_create (const char *name, int priority,
   tid = t->tid = allocate_tid ();
 
   // Initialize list of children status.
-  t->children_status = (struct list *)malloc(sizeof(struct list));
-  list_init(t->children_status);
+  list_init(&t->children_status);
 
-  // Add to parent if possible
-  if (thread_current()->children_status != NULL) {
-    list_push_back(thread_current()->children_status, &t->self_status->elem);
-  }
+  // Add to parent
+  list_push_back(&thread_current()->children_status, &t->self_status->elem);
 
   /* Stack frame for kernel_thread(). */
   kf = alloc_frame (t, sizeof *kf);
@@ -475,7 +472,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->magic = THREAD_MAGIC;
   #ifdef USERPROG
     list_init(&t->fileDescriptorList);
-    t->fileDesc = 2;
+    t->fileDesc = 3;
   #endif
 
   old_level = intr_disable ();

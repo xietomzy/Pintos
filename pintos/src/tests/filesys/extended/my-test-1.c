0f5e24ffb94cb68d6f8a61e9d6ba6d3f5f58d78a
John Angeles <johnangeles@berkeley.edu>
2020-05-06 04:52:49 +0000

    Changes

--- pintos/src/tests/filesys/extended/my-test-1.c
+++ pintos/src/tests/filesys/extended/my-test-1.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <syscall.h>
 #include "tests/filesys/extended/mk-tree.h"
 #include "tests/lib.h"
 #include "tests/filesys/extended/syn-rw.h"
 
+static char buf[BUF_SIZE];
+
 
 void test_main (void) {
-    char buf1[BUF_SIZE];
     int fd;
     int first_number_of_accesses;
     int second_number_of_accesses;
@@ -16,7 +16,17 @@ void test_main (void) {
 
     CHECK (create (file_name, BUF_SIZE), "create \"%s\"", file_name);
     CHECK ((fd = open (file_name)) > 1, "open \"%s\"", file_name);
+    for (int i = 0; i < sizeof buf; i++) 
+    {
+        char c;
+        // CHECK (read (fd, &c, 1) > 0, "read \"%s\"", file_name);
+        read (fd, &c, 1) > 0;
+    }
+    CHECK(number_cache_accesses() == 4096,"Number of bytes read: %i", 4096);
+    CHECK(number_cache_hits(),"Number of cache hits: %i", number_cache_hits());
 
-    msg ("Number of bytes read: %i", read(fd, buf1, sizeof buf1));
+    close()
+
+    // msg ("Number of bytes read: %i", read(fd, buf1, sizeof buf1));
 
 }

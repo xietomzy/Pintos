5b4a13cac56d745acec42da13c7c9a0657f2893a
Coby Zhong <coby.zhong@berkeley.edu>
2020-05-06 18:02:50 +0000

    Fix warnings by make

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -8,7 +8,8 @@ struct cache_block cache[MAX_CACHE_BLOCKS];
 
 struct cache_block *cache_get_block(block_sector_t sector); 
 void lru_move_front(struct cache_block *block);
-void new_block(struct block *device, struct cache_block *block, block_sector_t sector, void *buffer, bool r_or_w, off_t offset, int chunk_size);
+void new_block_read(struct block *device, struct cache_block *block, block_sector_t sector, void *buffer, off_t offset, size_t chunk_size);
+void new_block_write(struct block *device, struct cache_block *block, block_sector_t sector, const void *buffer, off_t offset, size_t chunk_size);
 /* Initialize the cache. */
 void cache_init (void) {
     list_init(&lru);
@@ -45,17 +46,21 @@ void lru_move_front(struct cache_block *block) {
     lock_release(&cache_lock);
 }
 /* Performs new block operations when block is pulled into the cache
-    0 for read, 1 for write */
-void new_block(struct block *device, struct cache_block *block, block_sector_t sector, void *buffer, bool r_or_w, off_t offset, int chunk_size) {
+*/
+
+void new_block_read(struct block *device, struct cache_block *block, block_sector_t sector, void *buffer, off_t offset, size_t chunk_size) {
     block_read(device, sector, block->data);
     block->valid = true;
     block->sector = sector;
-    if (r_or_w) { // write
-        memcpy(block->data + offset, buffer, chunk_size);
-        block->dirty = true;
-    } else { // read
-        memcpy(buffer, block->data + offset, chunk_size);
-    }
+    memcpy(buffer, block->data + offset, chunk_size);
+}
+
+void new_block_write(struct block *device, struct cache_block *block, block_sector_t sector, const void *buffer, off_t offset, size_t chunk_size) {
+    block_read(device, sector, block->data);
+    block->valid = true;
+    block->sector = sector;
+    memcpy(block->data + offset, buffer, chunk_size);
+    block->dirty = true;
 }
 
 void cache_read (struct block *block, block_sector_t sector, void *buffer, off_t offset, int chunk_size) {
@@ -89,7 +94,7 @@ void cache_read (struct block *block, block_sector_t sector, void *buffer, off_t
         if (!(cache[i].valid)) {
             lock_acquire(&(cache[i].cache_block_lock));
 
-            new_block(block, &cache[i], sector, buffer, 0, offset, chunk_size);
+            new_block_read(block, &cache[i], sector, buffer, offset, chunk_size);
             list_push_front(&lru, &(cache[i].elem));
     
             lock_release(&(cache[i].cache_block_lock));
@@ -106,14 +111,14 @@ void cache_read (struct block *block, block_sector_t sector, void *buffer, off_t
         block_write(block, evicted_blk->sector, evicted_blk->data);
     }
     /* Read in new block */
-    new_block(block, evicted_blk, sector, buffer, 0, offset, chunk_size);
+    new_block_read(block, evicted_blk, sector, buffer, offset, chunk_size);
     list_push_front(&lru, &(evicted_blk->elem));
 
     lock_release(&(evicted_blk->cache_block_lock));
     lock_release(&cache_lock);
 }
 
-void cache_write (struct block *block, block_sector_t sector, void *buffer, off_t offset, int chunk_size) {
+void cache_write (struct block *block, block_sector_t sector, const void *buffer, off_t offset, int chunk_size) {
     
     /* We must acquire a lock to start reading the cache. */
     lock_acquire(&cache_lock);
@@ -144,7 +149,7 @@ void cache_write (struct block *block, block_sector_t sector, void *buffer, off_
         if (!(cache[i].valid)) {
             lock_acquire(&(cache[i].cache_block_lock));
 
-            new_block(block, &cache[i], sector, buffer, 1, offset, chunk_size);
+            new_block_write(block, &cache[i], sector, buffer, offset, chunk_size);
             list_push_front(&lru, &(cache[i].elem));
 
             lock_release(&(cache[i].cache_block_lock));
@@ -161,7 +166,7 @@ void cache_write (struct block *block, block_sector_t sector, void *buffer, off_
         block_write(block, evicted_blk->sector, evicted_blk->data);
     }
 
-    new_block(block, evicted_blk, sector, buffer, 1, offset, chunk_size);
+    new_block_write(block, evicted_blk, sector, buffer, offset, chunk_size);
     list_push_front(&lru, &(evicted_blk->elem));
 
     lock_release(&(evicted_blk->cache_block_lock));

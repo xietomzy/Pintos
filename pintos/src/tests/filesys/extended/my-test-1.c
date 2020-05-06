7b255e9cebfc1133ee36cc28319f385b156c1545
John Angeles <johnangeles@berkeley.edu>
2020-05-06 05:48:26 +0000

    Having issues with creating tests and getting them to even run

--- pintos/src/tests/filesys/extended/my-test-1.c
+++ pintos/src/tests/filesys/extended/my-test-1.c
@@ -22,11 +22,21 @@ void test_main (void) {
         // CHECK (read (fd, &c, 1) > 0, "read \"%s\"", file_name);
         read (fd, &c, 1) > 0;
     }
-    CHECK(number_cache_accesses() == 4096,"Number of bytes read: %i", 4096);
-    CHECK(number_cache_hits(),"Number of cache hits: %i", number_cache_hits());
+    int num_old_hits = number_cache_hits();
+    CHECK(true, "Number of cache hits: %i", num_old_hits);
 
-    close()
+    CHECK(number_cache_accesses() == 4096, "Number of bytes read: %i", number_cache_accesses());
 
-    // msg ("Number of bytes read: %i", read(fd, buf1, sizeof buf1));
+    close(fd);
+
+    CHECK ((fd = open (file_name)) > 1, "open \"%s\"", file_name);
+        for (int i = 0; i < sizeof buf; i++) 
+    {
+        char c;
+        read (fd, &c, 1) > 0;
+    }
+
+    int num_new_hits = number_cache_hits() - num_old_hits;
+    CHECK(num_new_hits > num_old_hits, "Number of new hits: %i", num_new_hits);
 
 }

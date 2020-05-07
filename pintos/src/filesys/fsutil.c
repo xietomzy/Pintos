30d6eb6cb5c08742a3b308c7fd69ece8e5ea66a3
Tom Xie <xie.tom.zy@berkeley.edu>
2020-05-07 18:56:29 +0000

    added some changes to task 3, but still not passing all base tests

--- pintos/src/filesys/fsutil.c
+++ pintos/src/filesys/fsutil.c
@@ -39,8 +39,8 @@ fsutil_cat (char **argv)
   char *buffer;
 
   printf ("Printing '%s' to the console...\n", file_name);
-  bool *dummy;
-  file = filesys_open (file_name, dummy);
+  bool dummy;
+  file = filesys_open (file_name, &dummy);
   if (file == NULL)
     PANIC ("%s: open failed", file_name);
   buffer = palloc_get_page (PAL_ASSERT);
@@ -121,8 +121,8 @@ fsutil_extract (char **argv UNUSED)
           /* Create destination file. */
           if (!filesys_create (file_name, size))
             PANIC ("%s: create failed", file_name);
-          bool *dummy;
-          dst = filesys_open (file_name, dummy);
+          bool dummy;
+          dst = filesys_open (file_name, &dummy);
           if (dst == NULL)
             PANIC ("%s: open failed", file_name);
 
@@ -184,8 +184,8 @@ fsutil_append (char **argv)
     PANIC ("couldn't allocate buffer");
 
   /* Open source file. */
-  bool *dummy;
-  src = filesys_open (file_name, dummy);
+  bool dummy;
+  src = filesys_open (file_name, &dummy);
   if (src == NULL)
     PANIC ("%s: open failed", file_name);
   size = file_length (src);

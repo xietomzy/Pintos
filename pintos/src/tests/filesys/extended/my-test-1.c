2bf734721ffc114f7d0ee04a5b57b70ca303eb29
John Angeles <johnangeles@berkeley.edu>
2020-05-07 19:58:43 +0000

    Finished tests, feedback may be helpful

--- pintos/src/tests/filesys/extended/my-test-1.c
+++ pintos/src/tests/filesys/extended/my-test-1.c
@@ -4,6 +4,7 @@
 #include "tests/filesys/extended/syn-rw.h"
 #include <stdio.h>
 
+
 static char buf[BUF_SIZE];
 
 
@@ -37,8 +38,8 @@ void test_main (void) {
     /* Calculate the number of cache hits. */
     first_number_of_hits = number_cache_hits();
     first_number_of_accesses = number_cache_accesses(); 
-    msg("Number of first cache accesses: %i", first_number_of_accesses);
-    msg("Number of first cache hits: %i", first_number_of_hits);
+    msg("Number of first set of cache accesses: %i", first_number_of_accesses);
+    msg("Number of first set of cache hits: %i", first_number_of_hits);
 
     /* Close and reopen the file. */
     close(fd);
@@ -53,6 +54,6 @@ void test_main (void) {
     /* Find the number of cache hits this time. */
     second_number_of_hits = number_cache_hits() - first_number_of_hits;
     second_number_of_accesses = number_cache_accesses() - first_number_of_accesses;
-    msg("Number of total cache accesses: %i", second_number_of_accesses);
+    msg("Number of second set of cache accesses: %i", second_number_of_accesses);
     msg("Number of second set of cache hits: %i", second_number_of_hits);
 }
--- pintos/src/tests/filesys/extended/my-test-1.ck
+++ pintos/src/tests/filesys/extended/my-test-1.ck
@@ -9,10 +9,10 @@ check_expected (IGNORE_EXIT_CODES => 1, [<<'EOF']);
 (my-test-1) Number of initial cache hits: 0
 (my-test-1) create "logfile"
 (my-test-1) open "logfile"
-(my-test-1) Number of first cache accesses: 12395
-(my-test-1) Number of first cache hits: 12380
+(my-test-1) Number of first set of cache accesses: 12395
+(my-test-1) Number of first set of cache hits: 12380
 (my-test-1) open "logfile"
-(my-test-1) Number of total cache accesses: 12299
+(my-test-1) Number of second set of cache accesses: 12299
 (my-test-1) Number of second set of cache hits: 12299
 (my-test-1) end
 EOF

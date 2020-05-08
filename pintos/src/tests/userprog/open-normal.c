7a05ff31fffbc036536efe969bbf7d35e194946c
Coby Zhong <coby.zhong@berkeley.edu>
2020-05-08 17:47:40 +0000

    Pull skeleton tests

--- pintos/src/tests/userprog/open-normal.c
+++ pintos/src/tests/userprog/open-normal.c
@@ -7,11 +7,7 @@
 void
 test_main (void)
 {
-  number_device_reads();
-  number_device_writes();
   int handle = open ("sample.txt");
   if (handle < 2)
     fail ("open() returned %d", handle);
-  // reset_cache();
-
 }
